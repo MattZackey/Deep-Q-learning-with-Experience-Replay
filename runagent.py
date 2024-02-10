9b9604e320f0f1acabb58c0db56939e5cea322d4
Matthew Zackey <149171771+MattZackey@users.noreply.github.com>
2024-02-10 10:34:19 +0200

    Add files via upload

--- runagent.py
+++ runagent.py
@@ -15,22 +15,16 @@ if not os.path.exists(example_path ):
 frames = []
 env = gym.make("CartPole-v1", render_mode="rgb_array")
 observation, info = env.reset(seed=7896)
-
 agent_cart.policy_net.eval()
 state, info = env.reset()
 state = torch.tensor(state)
 done = False
-
-t = 0
 while not done:
     frames.append(env.render())
     action = torch.argmax(agent_cart.policy_net(state)).item()
     next_state, reward, terminated, truncated, _ = env.step(action)
     done = terminated or truncated
     state = torch.tensor(next_state)
-    t += 1
 env.close()
 
-print(t)
-
-imageio.mimsave(os.path.join(example_path , 'run2.gif'), frames, fps=30)
+imageio.mimsave(os.path.join(example_path , 'run800.gif'), frames, fps=30)
